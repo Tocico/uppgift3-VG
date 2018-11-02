a195acc0d08687704cf51d4fcb5d05a377531072
Tocico <kunotoshiko17@gmail.com>
2018-11-02 13:11:50 +0100

    new version

--- uppgift3-5.js
+++ uppgift3-5.js
@@ -5,16 +5,16 @@
         
         let randomArray = new Array(1000); // Skapat array som har 1000 utrymmet
     
-        for(let i= 0; i < randomArray.length; i++) // 1000 gånger slumpar tal mellan 1 och 6
+        for(let i= 0; i < randomArray.length; i++) // Medan loopar slumpar 1000 gånger slumpamässiga tal mellan 1 och 6
         {
-            randomArray[i] = Math.floor(Math.random()*6)+1;                                                
+            randomArray[i] = Math.floor(Math.random()*6)+1;   // Lägga 1000 slumpmässiga tal till randomArray                                        
         }
         
         return randomArray;
        
     
     }
-    randomDice();
+    
     
     console.log('------------Frekvenstabell---------------');
     
@@ -23,8 +23,7 @@
         let randomArray = randomDice(); // Anrop randomArray från randomDice()
 
         // Skapat randomPrint object för att lagra frekvenstabell
-        // randomPrint objekt som innehåller 6 olika egenskaper. 
-        //Dessutom har jag skapat en metoden som kan skriva ut resultatet
+        // randomPrint objekt som innehåller 6 olika egenskaper som kommer lagra antal av varje tal. 
         let randomPrint = {   
              one   : 0, 
              two   : 0,
@@ -32,14 +31,15 @@
              four  : 0,
              five  : 0,
              six   : 0,
-             printout : function(){
+             printout : function(){  //En metoden som kan skriva ut resultatet
                  return `1:${this.one}\n2:${this.two}\n3:${this.three}\n4:${this.four}\n5:${this.five}\n6:${this.six}\n`;
              }
         };
        
         // Medan loopar räknar det hur många av varje siffrorna som finns i randomArray
-       for(let i = 0; i < randomArray.length; i++){  
-           switch(randomArray[i]){ 
+       for(let i = 0; i < randomArray.length; i++){  //den går igenom 1000 slumpmässiga tal
+
+            switch(randomArray[i]){ // Sortera siffrorna
 
             case 1: randomPrint.one++; break;
             case 2: randomPrint.two++; break;
